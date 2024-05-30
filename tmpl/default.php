5f42fa66e673a75d2f378e1fbaa23b4c5ef8cdd5
TLWebdesign <info@tlwebdesign.nl>
2024-05-30 22:39:51 +0200

    Slider Stap 1

---
+++ tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_slider
+ *
+ * @copyright   Copyright (C) 2024 TLWebdesign. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+\defined('_JEXEC') or die;
+
+echo "Hallo JoomlaDagen!";
+?>
