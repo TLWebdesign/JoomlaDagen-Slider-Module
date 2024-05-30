315ea96a8ab6d65db75ec2bba20b9dac1e2b9f26
TLWebdesign <info@tlwebdesign.nl>
2024-05-30 23:16:39 +0200

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -10,6 +10,67 @@
 
 \defined('_JEXEC') or die;
 
-var_dump($slides);
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
+HTMLHelper::_('bootstrap.carousel', 'prettySliderCarousel' . $module->id);
+
+$slideCounter = 0;
+$autoPlay = $params->get("autoplay", null);
 ?>
+
+<div class="prettySliderWrapper">
+    <div id="prettySliderCarousel<?php echo $module->id; ?>"
+         class="carousel slide"
+         data-bs-ride="carousel"
+         style="max-height">
+        <div class="carousel-inner">
+            <?php
+            foreach ($slides as $slide) :
+                ?>
+                <div class="carousel-item <?php
+                echo ($slideCounter == 0) ? 'active' : ''; ?>"
+                     style="flex: 0 0 100%;">
+                    <div class="w-100 h-100">
+                        <img src="<?php
+                        echo $slide->image->url; ?>"
+                             class="w-100 h-auto"
+                             width="<?php echo $slide->image->width; ?>"
+                             height="<?php echo $slide->image->height; ?>"
+                        >
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5 class="text-white">
+                                <?php
+                                echo $slide->title; ?>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                $slideCounter++; ?>
+                <?php
+            endforeach; ?>
+        </div>
+        <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#prettySliderCarousel<?php
+                echo $module->id; ?>"
+                data-bs-slide="prev"
+        >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"><?php
+                echo Text::_('JPREVIOUS'); ?></span>
+        </button>
+        <button class="carousel-control-next"
+                type="button"
+                data-bs-target="#prettySliderCarousel<?php
+                echo $module->id; ?>"
+                data-bs-slide="next"
+        >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"><?php
+                echo Text::_('JNEXT'); ?></span>
+        </button>
+    </div>
+</div>
